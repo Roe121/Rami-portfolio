21f100824e0f0cda8294d34a39371f2073827cd5
Roe Lez <lezzarrami@gmail.com>
2023-11-27 10:54:22 +0100

    Files and directories set up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+import Contact from './Components/Contact';
+import Projects from './Components/Projects';
+import AboutMe from './Components/AboutMe';
+import Hero from './Components/Hero';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/hero">Accueil</Link>
+          </li>
+          <li>
+            <Link to="/apropos">À propos</Link>
+          </li>
+          <li>
+            <Link to="/projets">Projets</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contactez Moi</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+      <Route path="/hero"  component={Hero} />
+      <Route path="/apropos"  component={AboutMe} />
+      <Route path="/projets"  component={Projects} />
+      <Route path="/contact"  component={Contact} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;

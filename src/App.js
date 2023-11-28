fd0c4a44ace2abda49646edaf9995e42c82dbebc
Roe Lez <lezzarrami@gmail.com>
2023-11-28 14:18:33 +0100

    NavBar modification

--- src/App.js
+++ src/App.js
@@ -10,18 +10,24 @@ import { NavBarData } from './Data';
 function App() {
   return (
     <Router>
-      <nav className='bg-white shadow'>
+      <nav className='bg-white shadow flex items-center justify-center px-32 mx-auto'>
+        <div className="logo">
+          <span>LOGO</span>
+        </div>
+
         <ul className="container flex items-center justify-center p-6 mx-auto nav">
           
         {NavBarData.map((item)=>{
           return(
             <li>
-              <Link  to={item.link}>{item.name}</Link>
+              <Link className='hover:text-blue-800'  to={item.link}>{item.name}</Link>
             </li>
           )
         })}
-
         </ul>
+
+        <button>Telecharger le CV</button>
+
       </nav>
       <Routes>
       <Route path="/hero"  component={Hero} />

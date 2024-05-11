1f506e7ead6f8a3e0b35eba6480e2a44087c7dc5
Roe Lez <lezzarrami@gmail.com>
2024-05-11 23:38:25 +0200

    Skills section done

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,11 +1,10 @@
-import React, { useRef } from 'react';
+import React, { useRef } from "react";
 import "./Hero.css";
 import heroImg from "./last.png"; // Replace with your image filename
 
 const Hero = () => {
   return (
-    <section className="max-w-6xl mx-auto px-5 py-28">
-    <div className="hero-container ">
+    <div className="  py-28 overflow-hidden">
       <div class="firefly"></div>
       <div class="firefly"></div>
       <div class="firefly"></div>
@@ -21,18 +20,20 @@ const Hero = () => {
       <div class="firefly"></div>
       <div class="firefly"></div>
       <div class="firefly"></div>
-      <div className="hero-text">
-        <h1 className="text-[50px] font-mova font-extrabold">
-          Hello ! I am
-        </h1>
-        <span className="text-[50px] font-mova font-extrabold uppercase text-gray-600">
-          Rami
-        </span>
-        <p className="text-2xl">I'm a student and a passionate developer, eager to create innovative web solutions that make a </p>
+      <div className="hero-container ">
+        <div className="hero-text">
+          <h1 className="text-[50px] font-mova font-extrabold">Hello ! I am</h1>
+          <span className="text-[50px] font-mova font-extrabold uppercase text-gray-600">
+            Rami
+          </span>
+          <p className="text-2xl">
+            I'm a student and a passionate developer, eager to create innovative
+            web solutions that make a difference.
+          </p>
+        </div>
+        <img className="hero-image" src={heroImg} alt="Rami" />
       </div>
-      <img className="hero-image" src={heroImg} alt="Rami" />
     </div>
-    </section>
   );
 };
 
